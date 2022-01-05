2947ba94db331d39bae6cf683432a10e93931ad9
Dilshan <dilshan.mudali@gmail.com>
2022-01-04 19:17:21 -0500

    authentication done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const {check, validationResult} = require('express-validator')
+
+
 // @route GET api/auth
 // @desc Test route
 // @access public
@@ -13,6 +19,52 @@ router.get('/', auth, async (req,res) => {
         console.error(err.message)
         res.status(500).send('Server Error')
     }
+})
+
+// @route POST api/auth
+// @desc Authenticate user get token and login 
+// @access public
+router.post('/',[
+    check('email', 'Input a valid Email').isEmail(),
+    check('password', 'Please enter your password').exists()
+], async (req,res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    
+    const { email, password} = req.body
+    try {
+        //find user by email
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({errors: [{msg: 'Username or Password does not match records'}]})
+        }
+        //check if user password in database
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({errors: [{msg: 'Username or Password does not match records'}]})
+        }
+        //return jwt 
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 36000},
+            (err,token) => {
+                if(err) throw err;
+                res.json({token})
+            } 
+        )
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+    
 })
 
 module.exports = router
