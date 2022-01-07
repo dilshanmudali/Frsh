c4e7f889abb5fdadfc0e9f95903c0c5bca90d293
Dilshan <dilshan.mudali@gmail.com>
2022-01-06 19:24:21 -0500

    profile model

---
+++ models/Profile.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const ProfileSchema = new mongoose.ProfileSchema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    bio: {
+        type: String
+    },
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+        github: {
+            type: String
+        }
+    },
+    portfolio: {
+        type: String
+    },
+    employmentstatus: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
